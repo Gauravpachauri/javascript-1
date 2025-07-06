0a76d90ea34010ba0fa22d4b6ee5f2c41c0c105c
Gaurav pachauri <146556818+Gauravpachauri@users.noreply.github.com>
2025-07-06 10:06:59 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,12 @@
+const accountId=14453
+let accountEmail="abc@gmail.com"
+let accountPassword="12345"
+accountCity="jaipur"
+let accountState
 
+//accountId=2
+accountEmail="bcs@gmail.com"
+accountPassword="45678"
+accountCity="mathura"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+console.log("gaurav");
+let name="gaurav"
+let age=20
+let loggedin=false
+//number=2 to53
+//bigint
+//string=""
+//Boolean=true/false
+//null=standlone value
+//undefined=no value assign
+//symbol=unique
+console.log(typeof age);
+
